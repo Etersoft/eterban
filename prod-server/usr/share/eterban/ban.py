672fc9ad002605af121913006ee4977ddc0cde93
Ruzal Gimazov <diff@etersoft.ru>
2019-10-12 13:22:46 +0300

    Bug fixed

--- prod-server/usr/share/eterban/ban.py
+++ prod-server/usr/share/eterban/ban.py
@@ -5,9 +5,16 @@ f = open ('/etc/eterban/eterban.conf','r')
 line = f.readline()
 f.close()
 if line[:10] == "host_redis":
+    i = 10
+    while (line[i] == ' '):
+	i+=1
+    i+=1
+    while (line[i] == ' '):
+	i+=1
+
     if line[-1] == '\n':
-        host_redis = line[-16:-1]
+        host_redis = line[i:-1]
     else:
-        host_redis = line[-15:]
+        host_redis = line[i:]
 r = redis.Redis (host=host_redis)
 r.publish ('ban', sys.argv[1])
