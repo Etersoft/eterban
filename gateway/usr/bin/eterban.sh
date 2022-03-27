7a9389b4497a047ac9f42c24374b6ef3bb95d3a3
Vitaly Lipatov <lav@altlinux.ru>
2022-03-27 15:56:20 +0300

    add eterban cli for gateway target

---
+++ gateway/usr/bin/eterban.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+setname="eterban_1"
+
+command="$1"
+[ -n "$command" ] && shift
+
+if [ "$command" = "count" ] ; then
+    echo "Count of banned:"
+    ipset list $setname | wc -l
+    exit
+fi
+
+if [ "$command" = "list" ] ; then
+    ipset list $setname
+    exit
+fi
+
+if [ "$command" = "unban" ] ; then
+    /usr/share/eterban/unban.py $1
+    exit
+fi
+
+if [ "$command" = "search" ] ; then
+    mask="$(echo "$1" | sed -e 's|\.|\\.|g')"
+    ipset list $setname | grep --color "$mask"
+    exit
+fi
+
+cat <<EOF
+Usage:
+    eterban [count|list|search <ip>]
+
+       count       - print count of banned IPs
+       list        - list all banned IPs
+       search <ip> - search for ip in the list of banned IPs
+       unban <ip>  - unban IP
+EOF
