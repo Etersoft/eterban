ec53d777d8daa80b82bff5cba87985d1cbf1baec
Vitaly Lipatov <lav@etersoft.ru>
2026-07-20 22:52:50 +0300

    firehol: validate downloads before ipset swap

--- gateway/etc/cron.hourly/get_firehol_ip.sh
+++ gateway/etc/cron.hourly/get_firehol_ip.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
+set -eu
+
 firehol_name="firehol_level1"
 firehol_tmp="firehol_tmp"
-wget https://iplists.firehol.org/files/firehol_level1.netset -O $firehol_name
+workdir="$(mktemp -d /tmp/eterban-firehol.XXXXXX)"
+trap 'rm -rf "$workdir"' EXIT HUP INT TERM
 
-ipset create $firehol_tmp hash:net
+download="$workdir/firehol_level1.netset"
+wget --https-only --timeout=30 --tries=2 \
+    https://iplists.firehol.org/files/firehol_level1.netset -O "$download"
 
-( echo "create $firehol_tmp hash:net family inet hashsize 1024 maxelem 65536" ;\
-	cat $firehol_name | grep -v "^#" | sed -e "s|^\([0-9].*\)|add $firehol_tmp \1|" ) | ipset -exist restore
+test -s "$download"
+grep -Eq '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+(/[0-9]+)?$' "$download"
 
-ipset swap $firehol_tmp $firehol_name
-ipset destroy $firehol_tmp
+ipset destroy "$firehol_tmp" 2>/dev/null || :
+ipset create "$firehol_tmp" hash:net
+
+(
+    echo "create $firehol_tmp hash:net family inet hashsize 1024 maxelem 65536"
+    sed -n -E 's/^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+(\/[0-9]+)?)$/add '"$firehol_tmp"' \1/p' "$download"
+) | ipset -exist restore
+
+ipset swap "$firehol_tmp" "$firehol_name"
+ipset destroy "$firehol_tmp"
